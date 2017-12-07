f96394dd72269d76d600c09120045927dd161e2a
Oleg Stotsky <ostotsky@gmail.com>
2017-12-07 14:44:24 +0300

    style OptionModal component

--- src/components/Action.js
+++ src/components/Action.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Action = (props) => (
   <div>
-    <button class="big-button" disabled={!props.hasOptions} onClick={props.handlePick}>What should i do</button>
+    <button className="big-button" disabled={!props.hasOptions} onClick={props.handlePick}>What should i do</button>
   </div> 
 );
 
